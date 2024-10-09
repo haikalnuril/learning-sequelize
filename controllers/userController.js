3ae0ee8e5e5b2d34beb35f80da653670cb355886
haikalnuril <haikal.nuril23@gmail.com>
2024-10-09 12:49:46 +0700

    feat: update, delete, and create

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,4 +37,104 @@ const getUserById = async (req, res) => {
     }
 };
 
-module.exports = { getUsers, getUserById };
+const createUser = async (req, res) => {
+    try{
+        const { name, email, phone, isAdmin } = req.body
+
+        if(!name || !email || !phone || !isAdmin){
+            return res.status(404).json({
+                status: false,
+                "message": "name, email, phone, or isAdmin are required!"
+            })
+        }
+
+        const newUser = await user.create({
+            name,
+            email,
+            phone,
+            isAdmin
+        })
+
+        return res.status(201).json({
+            status: true,
+            message: "Create New User Successfully!",
+            data: newUser
+        })
+
+    } catch (err) {
+        return res.status(500).json({
+            status: false,
+            message: "An error occurred while fetching users",
+            error: err.message,
+        });
+    }
+}
+
+const updateUser = async (req, res) => {
+    try{
+        const getId = req.params.id
+        const {name, email, phone, isAdmin} = req.body
+
+        const userToUpdate = await user.findByPk(getId);
+
+        if (!userToUpdate) {
+            return res.status(404).json({
+                status: false,
+                message: "user not found!",
+            });
+        }
+
+        const updateUser = await userToUpdate.update({
+            name,
+            email,
+            phone,
+            isAdmin
+        })
+
+        res.status(200).json({
+            status: true,
+            message: "Update User Successfully!",
+            data: updateUser
+        });
+
+    } catch (err) {
+        return res.status(500).json({
+            status: false,
+            message: "An error occurred while fetching users",
+            error: err.message,
+        });
+    }
+}
+
+const deleteUser = async (req, res) => {
+    try{
+        const getId = req.params.id
+
+        const deleteUser = await user.destroy({
+            where:{
+                id : getId
+            }
+        })
+
+        if (deleteUser === 0) {
+            return res.status(404).json({
+                status: false,
+                message: "User Not Found!",
+            });
+        }
+
+        res.status(200).json({
+            status: true,
+            message: "User Deleted Successfully!",
+        });
+
+    } catch (err){
+        return res.status(500).json({
+            status: false,
+            message: "An error occurred while fetching users",
+            error: err.message,
+        });
+    }
+}
+
+module.exports = { getUsers, getUserById, createUser, updateUser, deleteUser };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router();
-const { getUsers, getUserById } = require("../controllers/userController")
+const { getUsers, getUserById, createUser, updateUser, deleteUser } = require("../controllers/userController")
 
 router.get("/", getUsers)
 router.get("/:id", getUserById)
+router.post("/", createUser)
+router.patch("/:id", updateUser)
+router.delete("/:id", deleteUser)
 
 module.exports = router
